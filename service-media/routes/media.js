183722336c17305aa1779b63a857cfe6f3a66a11
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-07 19:05:52 +0700

    membuat api upload image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const isBase64 = require("is-base64");
+const base64Img = require("base64-img");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const { Media } = require("../models");
+
+router.post("/", (req, res) => {
+  const image = req.body.image;
+
+  if (!isBase64(image, { mimeRequired: true })) {
+    return res.status(400).json({ status: "error", message: "Invalid base64" });
+  }
+
+  base64Img.img(image, "./public/images", Date.now(), async (err, filepath) => {
+    if (err) {
+      res.status(400).json({ status: "error", message: err.message });
+    }
+
+    const filename = filepath.split("\\").pop().split("/").pop();
+
+    const media = await Media.create({ image: `image/${filename}` });
+
+    return res.json({
+      status: "success",
+      data: {
+        id: media.id,
+        image: `${req.get("host")}/images/${filename}`,
+      },
+    });
+  });
 });
 
 module.exports = router;
