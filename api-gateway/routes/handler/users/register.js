169b642d7f55539ea8a0248a2c55192ead7606c0
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-08 15:10:36 +0700

    integrasi api register

---
+++ api-gateway/routes/handler/users/register.js
@@ -0,0 +1,21 @@
+const apiAdapater = require("../../apiAdapter");
+const { URL_SERVICE_USER } = process.env;
+
+const api = apiAdapater(URL_SERVICE_USER);
+
+module.exports = async (req, res) => {
+  try {
+    const user = await api.post("/users/register", req.body);
+    return res.json(user.data);
+  } catch (error) {
+    if (error.code === "ECONNREFUSED") {
+      return res.status(500).json({
+        status: "error",
+        message: "service unavailable",
+      });
+    }
+
+    const { status, data } = error.response;
+    return res.status(status).json(data);
+  }
+};
