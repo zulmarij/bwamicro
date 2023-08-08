3f6d7163b3231977f6b6954dbadb3de85ac299dc
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-08 16:50:30 +0700

    integrasi api get profile

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -8,5 +8,6 @@ const verifyToken = require('../middlewares/verifyToken')
 router.post("/register", usersHandler.register);
 router.post("/login", usersHandler.login);
 router.put("/", verifyToken, usersHandler.update);
+router.get("/", verifyToken, usersHandler.getUser);
 
 module.exports = router;
