0bcf493f2282ea328331a86c9ee87ad06896bdf0
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-08 15:46:01 +0700

    integrasi api login dan implementasi JWT

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersHandler = require("./handler/users");
 
 router.post("/register", usersHandler.register);
+router.post("/login", usersHandler.login);
 
 module.exports = router;
