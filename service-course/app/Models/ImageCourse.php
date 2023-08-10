41899ce4a521ef85e75b69a357fe4026a28663c0
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-10 10:16:57 +0700

    model imagecourse & mentor

---
+++ service-course/app/Models/ImageCourse.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImageCourse extends Model
+{
+    protected $table = 'image_courses';
+
+    protected $fillable = [
+        'course_id',
+        'image'
+    ];
+}
