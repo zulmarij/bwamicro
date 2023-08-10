0c5de6928d710d76b6450d4a2e4ed0856fc23102
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-10 11:30:16 +0700

    api mentor delete

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -20,3 +20,4 @@ Route::get('mentors', [MentorController::class, 'index']);
 Route::get('mentors/{id}', [MentorController::class, 'show']);
 Route::post('mentors', [MentorController::class, 'create']);
 Route::put('mentors/{id}', [MentorController::class, 'update']);
+Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
