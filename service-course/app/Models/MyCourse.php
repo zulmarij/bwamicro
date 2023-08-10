48068d4aa1578feaef89d666df8209b58a71af2f
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-10 10:21:53 +0700

    model mycourse & review

---
+++ service-course/app/Models/MyCourse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MyCourse extends Model
+{
+    protected $table = 'my_courses';
+
+    protected $fillable = [
+        'course_id',
+        'user_id'
+    ];
+
+    public function course() {
+        return $this->belongsTo('App\Course');
+    }
+}
