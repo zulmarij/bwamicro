f1913d79cd2c88102dba939928c7507d5f580120
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-11 10:03:24 +0700

    membuat function helpers

--- service-course/app/Helpers/helpers.php
+++ service-course/app/Helpers/helpers.php
@@ -1,6 +1,47 @@
 <?php
 
-function tes()
+use Illuminate\Support\Facades\Http;
+
+function getUser($userId)
 {
-    return 'just testing';
+    $url = env('SERVICE_USER_URL') . 'users/' . $userId;
+
+    try {
+        $response = Http::timeout(10)->get($url);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service user unavaliable'
+        ];
+    }
 };
+
+function getUserByIds($userIds = [])
+{
+    $url = env('SERVICE_USER_URL') . 'users/';
+
+    try {
+        if (count($userIds) === 0) {
+            return [
+                'status' => 'success',
+                'http_code' => 200,
+                'data' => []
+            ];
+        }
+
+        $response = Http::timeout(10)->get($url, ['user_ids[]' => $userIds]);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'service user unavaliable'
+        ];
+    }
+}
