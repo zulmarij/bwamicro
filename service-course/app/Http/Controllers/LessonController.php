4e3e481bb4c7c7c9596e77294ab8792ecdb8b263
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-11 08:30:43 +0700

    api create lesson

---
+++ service-course/app/Http/Controllers/LessonController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chapter;
+use App\Models\Lesson;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LessonController extends Controller
+{
+    public function create(Request $request)
+    {
+        $rules = [
+            'name' => 'required|string',
+            'video' => 'required|string',
+            'chapter_id' => 'required|integer',
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ]);
+        }
+
+        $chapterId = request()->input('chapter_id');
+        $chapter = Chapter::find($chapterId);
+        if (!$chapter) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'chapter not found'
+            ], 404);
+        }
+
+        $lesson = Lesson::create($data);
+        return response()->json([
+            'status' => 'success',
+            'data' => $lesson
+        ]);
+    }
+}
--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\LessonController;
 use App\Http\Controllers\MentorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -34,3 +35,5 @@ Route::get('chapters/{id}', [ChapterController::class, 'show']);
 Route::post('chapters', [ChapterController::class, 'create']);
 Route::put('chapters/{id}', [ChapterController::class, 'update']);
 Route::delete('chapters/{id}', [ChapterController::class, 'destroy']);
+
+Route::post('lessons', [LessonController::class, 'create']);
