7d6e49dabda4cb9ec374f4e54b3b4db8595ebefb
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-11 10:23:17 +0700

    api create my course

---
+++ service-course/app/Http/Controllers/MyCourseController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use App\Models\MyCourse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class MyCourseController extends Controller
+{
+    public function create(Request $request)
+    {
+        $rules = [
+            'course_id' => 'required|integer',
+            'user_id' => 'required|integer'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $courseId = $request->input('course_id');
+        $course = Course::find($courseId);
+        if (!$course) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'course not found'
+            ], 404);
+        }
+
+        $userId = $request->input('user_id');
+        $user = getUser($userId);
+        if ($user['status'] === 'error') {
+            return response()->json([
+                'status' => $user['status'],
+                'message' => $user['message']
+            ], $user['http_code']);
+        }
+
+        $isExistMyCourse = MyCourse::where('course_id', '=', $courseId)
+            ->where('user_id', '=', $userId)
+            ->exists();
+
+        if ($isExistMyCourse) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'user already taken this course'
+            ], 409);
+        }
+
+        $myCourse = MyCourse::create($data);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $myCourse
+        ]);
+    }
+}
--- service-course/app/Models/MyCourse.php
+++ service-course/app/Models/MyCourse.php
@@ -13,6 +13,11 @@ class MyCourse extends Model
         'user_id'
     ];
 
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+
     public function course() {
         return $this->belongsTo('App\Course');
     }
