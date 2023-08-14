85640f9d2f4dc5fd53ec5e5019cfbf0fa16d0420
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-14 10:20:40 +0700

    return status

--- api-gateway/middlewares/verifyToken.js
+++ api-gateway/middlewares/verifyToken.js
@@ -5,7 +5,7 @@ module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) {
-      res.status(403).json({
+      return res.status(403).json({
         status: "error",
         message: err.message,
       });
