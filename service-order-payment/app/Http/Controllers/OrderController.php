150231c77f4d496c5f0c055bf6b8f654ca1a2252
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2023-08-18 09:03:59 +0700

    change format order_id

--- service-order-payment/app/Http/Controllers/OrderController.php
+++ service-order-payment/app/Http/Controllers/OrderController.php
@@ -34,7 +34,7 @@ class OrderController extends Controller
         ]);
 
         $transactionDetails = [
-            'order_id' => $order['id'] . Str::random(5),
+            'order_id' => $order['id'] . '-' . Str::random(5),
             'gross_amount' => $course['price'],
         ];
         $itemDetails = [
